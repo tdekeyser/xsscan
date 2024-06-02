1cecd16b28fab08ef6d2a9fd812ce496efe154aa
tom <tom_dekeyser_7@hotmail.com>
2024-06-02 21:56:52 +0200

    init

--- README.md
+++ README.md
@@ -2,3 +2,8 @@
 
  Cross-site protection testing
  
+## TODO
+
+Read raw HTTP request and detect if:
+- the request uses session cookies
+- detect CSRF token presence
---
+++ src/main.rs
@@ -0,0 +1,13 @@
+use reqwest::header::CONTENT_SECURITY_POLICY_REPORT_ONLY;
+
+mod http;
+mod csrf;
+mod session;
+
+fn main() {
+
+    // let http_request = load_request(path);
+    // let detector = CsrfDetector::new();
+    // let result = detector.check(http_request);
+
+}
