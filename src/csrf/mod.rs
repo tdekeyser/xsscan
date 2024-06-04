efac9519a004952dae89655e16d9729637c64d91
tom <tom_dekeyser_7@hotmail.com>
2024-06-04 08:57:58 +0200

    some interfaces

--- src/csrf/mod.rs
+++ src/csrf/mod.rs
@@ -1,1 +1,23 @@
-pub mod token;
+use reqwest::Request;
+
+mod token;
+
+struct VulnerableRequest {}
+
+struct CsrfTester {}
+
+impl CsrfTester {
+    fn test_vulnerability(request: &Request) -> Option<VulnerableRequest> {
+        None
+    }
+}
+
+struct CsrfExploitGenerator {}
+
+struct CsrfExploit(String);
+
+impl CsrfExploitGenerator {
+    fn generate(request: VulnerableRequest) -> CsrfExploit {
+        CsrfExploit("".to_string())
+    }
+}
